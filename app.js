59c28dff86221db9cf9b7ea106097042c850d018
WaseemIshaq44 <waseemishaq44@gmail.com>
2025-01-10 16:10:41 +0500

    review

--- app.js
+++ app.js
@@ -5,28 +5,33 @@ function inputbutton(val){
 
     let inputfield=document.getElementById('inputfield');
     let lastValue=inputfield.value.slice(-1);
-    let lastTwoValue=inputfield.value.slice(-2);
     let lastThreeValue=inputfield.value.slice(-3);
     let lastSevenValue=inputfield.value.slice(-7);
 
     if (resetField && val !== "=") {
         inputfield.value = "";
         resetField = false;
-    }
+    } 
     if((lastValue==="+" || lastValue==="-" || lastValue==="*" || lastValue==="/") 
-        && (val==="+" || val==="-" || val==="*" || val==="/" || val==="**" || val==="**2" || val==="**(1/2)" )){
+        && (val==="+" || val==="-" || val==="*" || val==="/" || val==="**2" || val==="**(1/2)" )){
         inputfield.value=inputfield.value.slice(0,-1)+val;
-    }else if(lastTwoValue==="**" && (val==="+" || val==="-" || val==="*" || val==="/" || val==="**" || val==="**2" || val==="**(1/2)" )){
-        inputfield.value=inputfield.value.slice(0,-3)+val;       
     }else if(lastThreeValue==="**2" && (val==="+" || val==="-" || val==="*" || val==="/" || val==="**" || val==="**2" || val==="**(1/2)" )){
         inputfield.value=inputfield.value.slice(0,-3)+val;      
     }else if(lastSevenValue==="**(1/2)" && (val==="+" || val==="-" || val==="*" || val==="/" || val==="**" || val==="**2" || val==="**(1/2)" )){
         inputfield.value=inputfield.value.slice(0,-7)+val;
+    }else if(val==="%"){
+        inputfield.value=inputfield.value/100;
+        resetField=true;
+    }else if(val==="1/x"){
+        inputfield.value=1/inputfield.value;
+        resetField=true;
+    }else if(val==="C"){
+        inputfield.value="";
+        resetField=true;
     }else if(val==="="){
         inputfield.value=eval(inputfield.value);
         resetField=true;
-    }
-    else{
+    }else{
         inputfield.value+=val;
     }
 
